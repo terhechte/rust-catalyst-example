4caccd123cf4a93c898dfcc9a8c3e1828d87dfae
Benedikt Terhechte <terhechte@gmail.com>
2019-09-28 13:45:55 +0200

    Add xcframework support

--- make_fat.sh
+++ make_fat.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 # lipo together the different architectures into a universal 'fat' file
-xargo build --target x86_64-apple-ios-macabi --release
-xargo build --target aarch64-apple-ios --release
-lipo -create -output target/libtest1.a target/{aarch64-apple-ios,x86_64-apple-ios-macabi}/release/libtest1.a
+#xargo build --target x86_64-apple-ios-macabi --release
+#xargo build --target aarch64-apple-ios --release
+#xargo build --target x86_64-apple-ios --release
+
+set -x
+
+xcodebuild -create-xcframework \
+  -library target/aarch64-apple-ios/release/libtest1.a -headers test1_rust.h \
+  -library target/x86_64-apple-ios-macabi/release/libtest1.a -headers test1_rust.h \
+  -library target/x86_64-apple-ios/release/libtest1.a -headers test1_rust.h \
+  -output test1.xcframework
+
+set +x
+
