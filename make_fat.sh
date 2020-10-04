0958d85475b7d3cd47d48085972796af7b928859
Benedikt Terhechte <terhechte@gmail.com>
2020-10-04 08:48:04 +0200

    Go from xargo to using cargo with build-std

--- make_fat.sh
+++ make_fat.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# We need the SDK Root
+export SDKROOT=`xcrun --sdk macosx --show-sdk-path`
+
 # lipo together the different architectures into a universal 'fat' file
-xargo build --target x86_64-apple-ios-macabi --release
-xargo build --target aarch64-apple-ios --release
+# use `cargo build-std` to automatically build the std for non-tier1 platforms
+# such as catalyst
+cargo +nightly build -Z build-std --target x86_64-apple-ios-macabi --release
+cargo build --target aarch64-apple-ios --release
 lipo -create -output target/libtest1.a target/{aarch64-apple-ios,x86_64-apple-ios-macabi}/release/libtest1.a
