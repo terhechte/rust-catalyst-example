fe1fd56a3717b595b4207d41bc6b2d1e578c23ef
Benedikt Terhechte <terhechte@gmail.com>
2020-10-04 11:20:00 +0200

    Add Xcode sample project

---
+++ XcodeIntegration/XcodeIntegration/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XcodeIntegration
+//
+//  Created by terhechte on 04.10.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
